cc69abaf44a081779bf38dfcd12066008bb543f4
Brian Jacobo <jacobo.brian.fuentes@outlook.com>
2021-03-04 03:18:04 -0500

    commit 3

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
-const http = require('http')
+const express = require('express')
+const app = express()
+
+//servir contenido estatico
+app.use(express.static('public'))
+
+app.get('/', function (req, res) {
+  res.send('Home page')
+})
+
+app.get('/hola-mundo', function (req, res) {
+    res.send('Hello World in respective rute')
+  })
+ 
+app.listen(3000)
+
+
+/* const http = require('http')
 
 http.createServer((req,res)=>{
     
-    /* res.writeHead(200,{'Content-Type':'application/json'}) */
-    /* res.setHeader('Content-Disposition','attachment; filename=lista.csv')
+    res.writeHead(200,{'Content-Type':'application/json'})
+    res.setHeader('Content-Disposition','attachment; filename=lista.csv')
     res.writeHead(200,{'Content-Type':'application/csv'})
- */
+
     res.write('hola mundo')
     res.end()
 }).listen(8080)
 
-console.log('Escuchando en el puerto',8080);
+console.log('Escuchando en el puerto',8080); */
