6780a1f5cd61319df64c4bd74811faef87ee5e45
Brian Jacobo <jacobo.brian.fuentes@outlook.com>
2021-03-04 10:32:15 -0500

    commit 4 structute

--- app.js
+++ app.js
@@ -1,18 +1,34 @@
 const express = require('express')
 const app = express()
+const port = 3000
+
+
+// TODO: require('hbs')
+app.set('view engine','hbs')
 
 //servir contenido estatico
 app.use(express.static('public'))
 
-app.get('/', function (req, res) {
-  res.send('Home page')
+app.get('/', function(req,res){
+  res.send('hola mundo')
 })
 
-app.get('/hola-mundo', function (req, res) {
-    res.send('Hello World in respective rute')
+
+app.get('/generic', function (req, res) {
+    res.sendFile(__dirname+'/public/generic.html')
+  })
+
+  app.get('/elements', function (req, res) {
+    res.sendFile(__dirname+'/public/elements.html')
+  })
+
+  app.get('*', function (req, res) {
+    res.send(__dirname+'/public/404.html')
   })
  
-app.listen(3000)
+app.listen(port,()=>{
+  console.log(`Example app listening at http://localhost:${port}`);
+})
 
 
 /* const http = require('http')
